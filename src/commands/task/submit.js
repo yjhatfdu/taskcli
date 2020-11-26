dff493f98c9b361bcaffce95bd7d06e6c9aeeb87
yjhatfdu <yjhatfdu@gmail.com>
2020-11-26 17:16:06 +0800

    feat: support source mapping

--- src/commands/task/submit.js
+++ src/commands/task/submit.js
@@ -1,35 +1,18 @@
-const {Command, flags} = require('@oclif/command')
-const Request = require('../../utils/request')
-const { Color } = require('../../utils/color')
-const compileTs = require("../../compile");
-const requireFromString = require('require-from-string');
-const fs = require('fs')
-const path = require('path')
+require("source-map").SourceMapConsumer;
+const {Command, flags} = require('@oclif/command');
+const Request = require('../../utils/request');
+const {Color} = require('../../utils/color');
+const load = require('../../load');
 
 class TaskCommand extends Command {
-  run(){
-    const {flags} = this.parse(TaskCommand)
-    const filename = flags.filename
-
-    let fcontent = fs.readFileSync(filename).toString()
-    if (filename.endsWith(".ts")) {
-      const {code, errors} = compileTs(fcontent);
-      if (errors.length !== 0) {
-        console.error(errors.map(e => `${e.messageText}`).join('\n'))
-        process.exit();
-      }
-      fcontent = code[0].text
-    }
-
-    const ctx = requireFromString(fcontent, {
-      appendPaths: [path.resolve(__dirname, './../../../node_modules')],
-    })
-    const data = ctx.build()
-
-    Request('task','POST',data,(one) => {
-      const text = `ID => ${Color.Blue(one.ID)} | Version => ${Color.Green(one.Version)}`
-      console.log()
-      console.log(text)
+  async run() {
+    const {flags} = this.parse(TaskCommand);
+    const filename = flags.filename;
+    const data = await load(filename);
+    Request('task', 'POST', data, (one) => {
+      const text = `ID => ${Color.Blue(one.ID)} | Version => ${Color.Green(one.Version)}`;
+      console.log();
+      console.log(text);
       console.log()
     })
   }
@@ -37,6 +20,6 @@ class TaskCommand extends Command {
 
 TaskCommand.flags = {
   filename: flags.string({char: 'f', description: 'file to submit', required: true}),
-}
+};
 
-module.exports = TaskCommand
+module.exports = TaskCommand;
