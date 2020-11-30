82ffb410949df725a28084a0a2c6c707d31331e7
cao.anqi <cao.anqi@synyi.com>
2020-11-30 14:19:38 +0800

    update

--- src/commands/datasource/new.js
+++ src/commands/datasource/new.js
@@ -1,26 +1,26 @@
-const {Command} = require('@oclif/command')
-const Request = require('../../utils/request')
-const { Color } = require('../../utils/color')
+// const {Command} = require('@oclif/command')
+// const Request = require('../../utils/request')
+// const { Color } = require('../../utils/color')
 
-class TaskCommand extends Command {
-  static args = [
-    {name: 'name', required: true},
-    {name: 'type', required: true, options: ['postgres', 'oracle', 'sqlserver'],},
-    {name: 'url', required: true},
-  ];
+// class TaskCommand extends Command {
+//   static args = [
+//     {name: 'name', required: true},
+//     {name: 'type', required: true, options: ['postgres', 'oracle', 'sqlserver'],},
+//     {name: 'url', required: true},
+//   ];
 
-  run(){
-    const {args} = this.parse(TaskCommand);
-    var req = {
-      SourceType: args.type,
-      URL: args.url,
-      Name: args.name,
-    }
+//   run(){
+//     const {args} = this.parse(TaskCommand);
+//     var req = {
+//       SourceType: args.type,
+//       URL: args.url,
+//       Name: args.name,
+//     }
 
-    Request('datasource','Post',req,() => {
-      console.log(Color.Blue("Success"))
-    })
-  }
-}
+//     Request('datasource','Post',req,() => {
+//       console.log(Color.Blue("Success"))
+//     })
+//   }
+// }
 
-module.exports = TaskCommand
+// module.exports = TaskCommand
